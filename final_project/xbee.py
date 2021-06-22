1a28b1d34e805d084b8d1f1796d28a7d450ad634
blair488 <samantha99fan@gmail.com>
2021-06-22 19:54:51 +0800

    update

--- final_project/xbee.py
+++ final_project/xbee.py
@@ -1,15 +1,13 @@
+
 import serial
 import time
-import string
+import sys,tty,termios
 
 # XBee setting
 serdev = '/dev/ttyUSB0'
 s = serial.Serial(serdev, 9600)
 
 while True:
-    char_num = s.read(3)
-    char_num = atoi(char_num)
-    rsv = s.read(char_num)
-    print(rsv.decode())
-
-s.close()
+    if(s.readable):
+        rsv = s.readline()
+        print(rsv.decode())
